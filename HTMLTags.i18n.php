049a1bfc16a6f73a6f1e1ff22a3ae2936bcf676f
yaron <yaron57@gmail.com>
2012-11-01 20:03:58 +0300

    New extension: "HTML Tags"
    Change-Id: I10280930e0eaf97015e62e88809a785892683dc2

---
+++ HTMLTags.i18n.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Messages file for the HTML Tags extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+/**
+ * Get all extension messages
+ *
+ * @return array
+ */
+$messages = array();
+
+$messages['en'] = array(
+    'htmltags-desc' => 'Allows for displaying HTML tags from a pre-specified set.',
+    'htmltags-notagname' => 'The attribute "tagname" must be set for this tag.',
+    'htmltags-unsupportedtag' => 'The tag name "$1" is not supported for <htmltag>.',
+);
